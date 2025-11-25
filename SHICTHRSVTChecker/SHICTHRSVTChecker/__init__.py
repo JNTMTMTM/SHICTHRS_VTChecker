6cabbdb7f087bbf2b2db39939326da061b342be0
JNTMTMTM <15251865468@163.com>
2025-11-25 22:21:01 +0800

    V0.0.0 添加数据执行保护状态检查功能并修复错误代码

--- SHICTHRSVTChecker/SHICTHRSVTChecker/__init__.py
+++ SHICTHRSVTChecker/SHICTHRSVTChecker/__init__.py
@@ -10,6 +10,7 @@ from colorama import init
 init()
 
 from .utils.api.SHRVChecker_get_virtualization_firmware_stu import SHRVChecker_get_virtualization_firmware_stu
+from .utils.api.SHRVChecker_get_system_dep_stu import SHRVChecker_get_system_dep_stu
 
 print('\033[1mWelcome to use SHRVTChecker - VT info checker\033[0m\n|  \033[1;34mGithub : https://github.com/JNTMTMTM/SHICTHRS_VTChecker\033[0m')
 print('|  \033[1mAlgorithms = rule ; Questioning = approval\033[0m')
@@ -26,7 +27,10 @@ def SHRVTChecker_get_vt_info() -> dict:
     # VirtualizationFirmware
     try:
         VirtualizationFirmware_stu : dict = SHRVChecker_get_virtualization_firmware_stu(SHRVTCheckerException)
+        DataExecutionPrevention_stu : dict = SHRVChecker_get_system_dep_stu(SHRVTCheckerException)
 
+        return {'VirtualizationFirmware' : VirtualizationFirmware_stu ,
+                'DataExecutionPrevention_stu' : DataExecutionPrevention_stu}
 
     except Exception as e:
         raise SHRVTCheckerException(f"SHRVTCheckerException [ERROR.5000] unable to get VirtualizationFirmwareEnabled info. | {str(e)}")
--- SHICTHRSVTChecker/SHICTHRSVTChecker/utils/api/SHRVChecker_get_virtualization_firmware_stu.py
+++ SHICTHRSVTChecker/SHICTHRSVTChecker/utils/api/SHRVChecker_get_virtualization_firmware_stu.py
@@ -6,7 +6,7 @@ def SHRVChecker_get_virtualization_firmware_stu(error_class) -> dict:
         VirtualizationFirmware_stu : dict = run_powershell_command(error_class , ["wmic", "cpu", "get", "VirtualizationFirmwareEnabled"])
         
         if (not isinstance(VirtualizationFirmware_stu , dict)) or str(list(VirtualizationFirmware_stu.keys())[0]).lower() != 'virtualizationfirmwareenabled':
-            raise error_class(f"SHRVTCheckerException [ERROR.5001.1] an invalid parameter was output")
+            raise error_class(f"SHRVTCheckerException [ERROR.5001.0] an invalid parameter was output")
             
         return VirtualizationFirmware_stu
     except Exception as e:
